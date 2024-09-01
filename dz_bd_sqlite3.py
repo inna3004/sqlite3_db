d8693a555bbd6934a1e4b31c1a2d8831453ec697
inna_ <inna_isachenko@mail.ru>
2024-09-01 16:14:30 +1000

    sqlite

--- dz_bd_sqlite3.py
+++ dz_bd_sqlite3.py
@@ -1,19 +1,22 @@
-import  sqlite3 as sl
+import sqlite3 as sl
 
 con = sl.connect('thecode.db')
 
 with con:
     con.execute("""
     CREATE TABLE account(
-    user_id serial PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    email VARCHAR(355) UNIQUE NOT NULL,
-    created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    last_login TIMESTAMP
+        user_id serial PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(50) NOT NULL,
+        email VARCHAR(355) UNIQUE NOT NULL,
+        created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        last_login TIMESTAMP
     );
-    CREATE TABLE table name (
-   column_name TYPE column_constraint,
-   table_constraint table_constraint
-     ) INHERITS exiting_table_name;
+    """)
+
+    con.execute("""
+    CREATE TABLE inna(
+        column_name TYPE column_constraint,
+        table_constraint table_constraint  
+        INHERITS account);
     """)
